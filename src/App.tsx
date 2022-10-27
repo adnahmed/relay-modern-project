62f8c85c1096cdb7d6e32ed16116a9d7eebb44ab
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-10-28 01:24:04 +0500

    OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 import create from 'zustand';
-
-interface TodoState {
-  votes: number
-  addVotes: () => void
-  substractVotes: () => void
-  action: () => void
+interface User {
+    name: string
+    id: string // UUID type
 }
-// define store
-const useStore = create<TodoState>((set, get) => ({
-  votes: 0,
-  action: () => {
-    const userVotes = get().votes
-  },
-  addVotes: () => set(state => ({votes: state.votes + 1})),
-  substractVotes: () => set(state => ({votes: state.votes - 1}))
+const useUserStore = create<User>((set) => ({
+    name: "", 
+    id: "",
+    fetch: async () => {    
+       const response = await fetch(process.env.REACT_APP_SERVER + "/user/");
+       set({ ...await response.json() })
+    },
+    isLoggedIn: true,
+    login: () => set((state) => ({ ...state, isLoggedIn: true })),
+    logout: () => set((state) => ({...state, isLoggedIn: false }))
 }));
+interface Farm {
+    address: string
+    name: string
+}
 
 function App() {
-  const addVotes = useStore(state => state.addVotes);
-  const substractVotes = useStore(state => state.substractVotes)
-  const getVotes = useStore((state) => state.votes);
-  return (
-    <div className="App">
-      <h1>{getVotes} people have cast their votes.</h1>
-      <button onClick={addVotes}>Cast a vote</button>
-      <button onClick={substractVotes}>Delete a vote.</button>
-    </div>
-  );
+        const fetchUser = useUserStore((state) => state.fetch);
+        useEffect(() => {
+            fetchUser();
+        })
+
+        return (
+        <div className="App">
+            <div className="Banner">
+                <button className="BannerName"> Farms</button>
+                <div className="BannerGap"></div>
+                <img className="BannerImage" src={process.env.REACT_APP_SERVER! + "/farm/1/flag"} alt="flag"/>
+            </div>
+            <MainBanner />
+            <div className="AppName">
+                Kesan Diary
+            </div>
+
+        </div>
+    );
+}
+
+
+function MainBanner() {
+    var [farm, setFarm] = useState<Farm>();
+    useEffect(() => {
+        fetch("http://localhost:3000/farm/")
+            .then(res => res.json())
+            .then(res => setFarm(res))
+    }, []);
+    return (<><div className="MainName">{farm?.name}</div>
+    <div className="BannerAddress">{farm?.address}</div></>);
 }
 
 export default App;
