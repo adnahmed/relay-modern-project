5b30dc84fb59f7dc73cdd90dec6c0b12c054133d
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-10-30 16:31:06 +0500

    Base Table for LandPreparationAndSowing

--- src/pages/LandPreparationAndSowingCard/LandPreparationAndSowingCard.tsx
+++ src/pages/LandPreparationAndSowingCard/LandPreparationAndSowingCard.tsx
@@ -1,5 +1,16 @@
-import React from "react";
-import {CellRow} from '../../Components/CellRow'
+import React from 'react'
+import { CellRow } from '../../Components/CellRow'
 export function LandPreparationAndSowingCard() {
-    return (<CellRow values={["Operation/Activity", "Best Practices", "Conventional Practices"]}/>);
+  return (
+    <>
+      <CellRow style={{ fontWeight: 'bold' }} values={['Operation/Activity', 'Best Practices', 'Conventional Practices']} />
+      <CellRow values={['']} />
+      <CellRow values={[`Total Land (${'land'})`, `${'bestPracticesLand'}`, `${'conv.Land'}`]} />
+      <CellRow style={{ fontWeight: 'bold' }} values={['', <CellRow values={['Quantity', 'Total Cost PKR']} />, <CellRow values={['Quantity', 'Total Cost PKR']} />]} />
+      <CellRow cellStyle={{ textAlign: 'left' }} style={{ fontWeight: 'bold' }} values={['A. Land Preparation (Own tractor and machinery)']} />
+      <CellRow values={['Land preparation (disk plough, cultivator, suhaga, chesil, rotavator)', <CellRow values={['', '']} />, <CellRow values={['', '']} />]} />
+      <CellRow values={['Planter', <CellRow values={['', '']} />, <CellRow values={['', '']} />]} />
+      <CellRow style={{ fontWeight: 'bold'}} values={['Gross Land Preparation Cost', <CellRow values={['', '']} />, <CellRow values={['', '']} />]} />
+    </>
+  )
 }
