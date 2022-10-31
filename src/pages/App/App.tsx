541ccab40f3d42723b9a0eb7dfc7f936ca880aee
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-10-31 13:23:42 +0500

    User Auth Form

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,30 +1,60 @@
 import './App.css'
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import {CropCard} from "../CropCard/CropCard";
-import {LandPreparationAndSowingCard} from "../LandPreparationAndSowingCard/LandPreparationAndSowingCard";
-import {NoMatchCrop} from "../../Models/Crop";
-import {Dashboard} from "../Dashboard/Dashboard";
-import {Layout} from "../../Layout/Layout";
-import {NoMatch} from "../../Components/NoMatch";
+import React, { useState, useRef } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { CropCard } from '../CropCard/CropCard'
+import { LandPreparationAndSowingCard } from '../LandPreparationAndSowingCard/LandPreparationAndSowingCard'
+import { NoMatchCrop } from '../../Models/Crop'
+import { Dashboard } from '../Dashboard/Dashboard'
+import { Layout } from '../../Layout/Layout'
+import { NoMatch } from '../../Components/NoMatch'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [password, setPassword]=useState("")
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="/crop/:cropName">
-            <Route index element={<CropCard />} />
-            <Route path="land-preparation-and-sowing" element={<LandPreparationAndSowingCard />} />
-            <Route path="*" element={<NoMatchCrop />} />
+      {isAuthenticated ? (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/crop/:cropName">
+              <Route index element={<CropCard />} />
+              <Route path="land-preparation-and-sowing" element={<LandPreparationAndSowingCard />} />
+              <Route path="*" element={<NoMatchCrop />} />
+            </Route>
+            <Route path="*" element={<NoMatch />} />
           </Route>
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
+        </Routes>
+      ) : (
+        // <AuthenticationForm />
+        <div>
+          <form method="get">
+            <fieldset>
+              <legend>Authentication</legend>
+              <label>
+                Email
+                <input placeholder="Enter your email" name="User Email" required value="" type="text"></input>
+              </label>
+              <label>
+                Password
+                <input placeholder="Enter your password" name="User Password" required value={password} type="password"></input>
+              </label>
+            </fieldset>
+            <button>Login</button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
 
-
+function AuthenticationForm() {
+  return (
+    <div className="Form AuthenticationForm">
+      <form target="/">
+        <input type={'text'}>Please Enter Email.</input>
+      </form>
+    </div>
+  )
+}
 export default App
