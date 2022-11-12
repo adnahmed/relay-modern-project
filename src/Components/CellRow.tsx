1199745348c61df58e26a9d69897a82ea9c2de7d
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-11-12 20:32:32 +0500

    formatting CellRow

--- src/Components/CellRow.tsx
+++ src/Components/CellRow.tsx
@@ -2,33 +2,34 @@
 A stub interface, use react-props here.
 */
 import React from 'react'
-import { Cell } from './Cell'
+import {Cell} from './Cell'
 
 const cellRowStyle = (isVertical: boolean, cells: number) => {
-  return isVertical ? { gridTemplateRows: `repeat(${cells}, 1fr)` } :
-        { gridTemplateColumns: `repeat(${cells}, 1fr)`}
+    return isVertical ? {gridTemplateRows: `repeat(${cells}, 1fr)`} :
+        {gridTemplateColumns: `repeat(${cells}, 1fr)`}
 }
+
 // TODO: Validate props with react-props
 export function CellRow(props) {
-  return (
-    <div
-      className="CellRow"
-      style={{
-          ...cellRowStyle(props.isVertical, props.values.length),
-          ...props.style,
-        display: 'grid',
-      }}
-    >
-      {props.values.map((p) => {
-        switch (typeof p) {
-          case 'string':
-            return <Cell style={props.cellStyle} value={p} />
-          case 'object':
-            return p
-          default:
-            return console.log('Unknown Object Received. CellRow only accepts values of type (string|CellRow)[].')
-        }
-      })}
-    </div>
-  )
+    return (
+        <div
+            className="CellRow"
+            style={{
+                ...cellRowStyle(props.isVertical, props.values.length),
+                ...props.style,
+                display: 'grid',
+            }}
+        >
+            {props.values.map((p) => {
+                switch (typeof p) {
+                    case 'string':
+                        return <Cell style={props.cellStyle} value={p}/>
+                    case 'object':
+                        return p
+                    default:
+                        return console.log('Unknown Object Received. CellRow only accepts values of type (string|CellRow)[].')
+                }
+            })}
+        </div>
+    )
 }
