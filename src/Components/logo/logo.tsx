942cc52cad5cb9da935b05439a63b91e141eea9b
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-11-19 10:37:20 +0500

    (fix): remove code for polaris img object

--- src/Components/logo/logo.tsx
+++ src/Components/logo/logo.tsx
@@ -1,9 +1,2 @@
 import AppLogo from './logo.webp'
-const env = process.env;
-const logo = {
-  width: 54,
-  topBarSource: AppLogo,
-  url: `${env.NODE_ENV === 'production' ? env.APP_URL : env.REACT_APP_SERVER || `localhost:${env.PORT || 3000}`}`,
-  accessibilityLabel: 'Kesan Diary',
-}
-export default logo
+export default AppLogo
