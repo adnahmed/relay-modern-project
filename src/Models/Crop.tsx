89e8ef4527fb758c1a7ea48a9331a1866af7962e
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-08 16:56:31 +0500

    (feat): Crop Model

--- src/Models/Crop.tsx
+++ src/Models/Crop.tsx
@@ -1,9 +1,12 @@
-export interface Crop {
-    id: string
-    name: string
-    landOccupied?: number
+export class Crop {
+    id: string = ''
+    fullName: string = ''
+    landOccupied: number = 0
 }
-
+export class LandType {
+    id: string = ''
+    name: string = ''
+}
 export interface Crops {
     crops: Crop[]
 }
