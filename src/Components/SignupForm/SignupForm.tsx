ca7424f9f6bba3296560bf73eb0358d33863a606
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-08 16:56:16 +0500

    (fix): changes in select component

--- src/Components/SignupForm/SignupForm.tsx
+++ src/Components/SignupForm/SignupForm.tsx
@@ -7,6 +7,7 @@ import CheckboxFormik from '../Input/CheckboxFormik/CheckboxFormik'
 import './SignupForm.scss'
 import { useAuth } from '../../Hooks/useAuth'
 import { useNavigate } from 'react-router-dom'
+import ALL_CITIES from '../../Models/Constant/AllCountries';
 
 interface SignupFormProps {}
 const InputValues: Record<string, string | boolean> = {
@@ -22,7 +23,6 @@ const InputValues: Record<string, string | boolean> = {
   country: '',
   acceptedTerms: false,
 }
-
 const SignupForm: FC<SignupFormProps> = () => {
   const auth = useAuth()
   const navigate = useNavigate()
@@ -37,11 +37,8 @@ const SignupForm: FC<SignupFormProps> = () => {
           mobileOrEmail: Yup.string().email('Invalid email address.').required('Required'),
           newPassword: Yup.string().required('Required'),
           repeatPassword: Yup.string().required('Required'),
-          job: Yup.string().oneOf(['Farm Manager', 'Farmer', 'Expert', 'Other']).required('Required'),
-          otherJob: Yup.string().when('job', { is: true, then: Yup.string().required('A Job Title is Required.') }),
           permanentAddress: Yup.string().required('Required'),
           city: Yup.string().required('Required'),
-          country: Yup.string().required('Required'),
           acceptedTerms: Yup.boolean().required('Required').oneOf([true, false], 'You must accept Terms and Conditions.'),
         })}
         onSubmit={(values, actions) => {
@@ -58,24 +55,7 @@ const SignupForm: FC<SignupFormProps> = () => {
             <TextInputFromik label="Password" name="newPassword" type="text" hidden />
             <TextInputFromik label="Repeat Password" name="repeatPassword" type="text" hidden />
             <TextInputFromik label="Address" name="permanentAddress" type="text" />
-            <SelectFormik label="Job" name="job">
-              <option value="">Select Job</option>
-              <option value="Farm Manager">Farm Manager</option>
-              <option value="Expert">Expert</option>
-              <option value="Farmer">Farmer</option>
-              <option value="Other">Other</option>
-            </SelectFormik>
-            {props.values.job === 'Other' && <TextInputFromik label="Other Job" name="otherJob" type="text" />}
-            <SelectFormik label="Country" name="country">
-              <option value="">Select Country</option>
-              <option value="rwp">Pakistan</option>
-              <option value="isl">India</option>
-            </SelectFormik>
-            <SelectFormik label="City" name="city">
-              <option value="">Select City</option>
-              <option value="rwp">Rawalpindi</option>
-              <option value="isl">Islamabad</option>
-            </SelectFormik>
+            <SelectFormik label="City" name="city" options={ALL_CITIES}/>
             <CheckboxFormik name="acceptedTerms">
               I accept the <a className="TermsAndConditions">Terms and Conditions</a>
             </CheckboxFormik>
