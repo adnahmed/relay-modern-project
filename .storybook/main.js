9747a1e81e9f92d726ef3ac5798d63d4543ddf2e
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-09 19:54:55 +0500

    (fix): add storybook-postcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,25 @@
+const path = require('path')
+const tailwindConfigPath = path.join(__dirname, '../tailwind.config.js')
+require('storybook-tailwind-foundations/initialize.js').default(tailwindConfigPath)
+
 module.exports = {
-    stories: ['../src/{Components,pages}/**/*.stories.tsx'],
-    staticDirs: ['../public'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/preset-create-react-app',
-        '@storybook/addon-interactions',
-        'storybook-vscode-component/register'
-        // [require.resolve('storybook-addon-grid')]
-    ],
-    framework: '@storybook/react',
-    core: {
-        builder: '@storybook/builder-webpack5',
-    },
-    features: {
-        interactionsDebugger: true,
-    },
+  stories: ['../src/{Components,pages}/**/*.stories.tsx', '../node_modules/storybook-tailwind-foundations/**/*.stories.js'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    'storybook-vscode-component/register',
+    'storybook-addon-sass-postcss',
+    'storybook-tailwind-dark-mode',
+    // [require.resolve('storybook-addon-grid')],
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  features: {
+    interactionsDebugger: true,
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,25 +2,27 @@ import ReactRouterDecorator from './ReactRouterDecorator'
 import { wd } from 'paths.macro'
 /// Configure Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 const parameters = {
-    actions: {argTypesRegex: '^on[A-Z].*'},
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
-    grid: {
-        gridOn: true,
-        columns: 12,
-        gap: '20px',
-        gutter: '50px',
-        maxWidth: '1024px',
-    },
-    storySource: {
-        repository: 'https://github.com/adnahmed/react-app',
-        workingDir: wd || 'D:\\projects\\react-app\\'
-
-    }
-};
+  },
+  // grid: {
+  //     gridOn: true,
+  //     columns: 12,
+  //     gap: '20px',
+  //     gutter: '50px',
+  //     maxWidth: '1024px',
+  // },
+  storySource: {
+    repository: 'https://github.com/adnahmed/react-app',
+    workingDir: wd || 'D:\\projects\\react-app\\',
+  },
+}
 export const decorators = [ReactRouterDecorator]
+export const globalType = {
+  darkMode: true,
+}
 export default parameters
