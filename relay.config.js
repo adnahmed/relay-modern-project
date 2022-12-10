61e7e6b83a1f2982b4fd815e4e7013c9a996db40
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-10 11:56:36 +0500

    (fix): use js for relay config

---
+++ relay.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  src: './src',
+  language: 'typescript',
+  schema: './schema.graphql',
+  excludes: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
+  artifactDirectory: './.graphql',
+}
