b0bda4fb3219ad718dfcb951c752d896e27445e9
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-10 20:56:31 +0500

    (build): add eslint-relay rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,21 @@ module.exports = {
         schemaJson: require('./schema.json'),
       },
     ],
+    'relay/graphql-syntax': 'error',
+    'relay/compat-uses-vars': 'warn',
+    'relay/graphql-naming': 'error',
+    'relay/generated-flow-types': 'warn',
+    'relay/must-colocate-fragment-spreads': 'warn',
+    'relay/no-future-added-value': 'warn',
+    'relay/unused-fields': 'warn',
+    'relay/function-required-argument': 'warn',
+    'relay/hook-required-argument': 'warn'
   },
   overrides: [
     {
       files: ['**/*.stories.*'],
     },
   ],
-  plugins: ['graphql', 'tailwindcss'],
+  plugins: ['graphql', 'tailwindcss', 'relay'],
   extends: ['react-app', 'react-app/jest', 'plugin:storybook/recommended', 'plugin:tailwindcss/recommended', 'plugin:relay/recommended'],
 }
