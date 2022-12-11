858afbfd8a63d3ab971d779de7b21fa54ee8dce4
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-11 19:03:19 +0500

    (feat): Cookies Provider on top

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import './index.scss'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie'
 import reportWebVitals from './reportWebVitals'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import App from './pages/App/App'
-import { ProvideAuth } from './Hooks/useAuth'
+
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container!)
 root.render(
   <BrowserRouter>
-    <ProvideAuth>
+    <CookiesProvider>
       <App />
-    </ProvideAuth>
+    </CookiesProvider>
   </BrowserRouter>
 )
 // If you want to start measuring performance in your app, pass a function
