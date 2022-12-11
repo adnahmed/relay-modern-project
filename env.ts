b48d9fef37ce074d326163d1be5dd526853e00af
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-11 19:03:00 +0500

    (feat): JWT envars added

--- env.ts
+++ env.ts
@@ -1,7 +1,10 @@
-import { cleanEnv, url } from 'envalid'
+import { cleanEnv, str, url } from 'envalid'
 
+const isDevelopment = process.env.NODE_ENV === 'development'
 const env = cleanEnv(process.env, {
-    SCHEMA_ENDPOINT: url({ default: 'http://localhost:4000/graphql'})
+  SCHEMA_ENDPOINT: url({ default: 'http://localhost:4000/graphql' }),
+  JWT_SECRET: str({ default: isDevelopment ? 'mysecret' : undefined }),
+  JWT_EXPIRE: str({ default: '7d' }),
 })
 
-export default env;
+export default env
